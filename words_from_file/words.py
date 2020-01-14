32c12b0d97336b5b41b3f0b5bef351f81e1bf212
Paulina Grajewska <56506556+ZaulaGrajla@users.noreply.github.com>
2020-01-14 18:17:55 +0100

    comments delete

--- words_from_file/words.py
+++ words_from_file/words.py
@@ -1,17 +1,9 @@
 from random import randint
 import os
 
-
-# wczytać zawartość pliku (może do listy?)
-# wylosować słowo z listy albo innej takiej
-# zwrócić wylosowane słowo
-# co to znaczy parsować
-
 class Words:
 
     def __init__(self, name_of_file='words_from_file/words.txt'):
-        # def __init__(self, name_of_file='words_from_file/words.txt'):
-        # def __init__(self, name_of_file='words.txt'):
         self.file = self.get_path()
 
     def get_path(self):
@@ -22,23 +14,14 @@ class Words:
     def get_word(self):
         lines = 0
         with open(f'{self.get_path()}/words.txt', 'r') as plik:
-            # self.list_of_words = plik.read().split('\n')
             while plik.readline():
                 lines += 1
-
-        # random_line = lines  # randint(0,lines-1)
-        # print(random_line is lines)
-        # print(lines, random_line)
         word = None
         random_line = randint(1, lines)
         lines = 0
-        # print(random_line is lines)
         with open(f'{self.get_path()}/words.txt') as file:
             for i in range(random_line):
-                # while file.readline() and random_line > lines:
-                #     lines += 1
-                word = file.readline().strip()  # co to?usuwa ostatni nieprintowalny znak, usuwa białe znaki na początku i na końcu
-
+                word = file.readline().strip()
             print(word, len(word))
         return word
 
@@ -58,20 +41,8 @@ def get_word_from_file():
         while line_read < random_line:
             line_read += 1
             our_word = file.readline().strip()
-            # print(our_word)
         print(our_word)
-
-
-# def get_word(self) -> str:
-#     index = randint(0, len(self.list_of_words) - 1)
-#     return self.list_of_words[index]
-
-
-# dupa = Words()
-# print(dupa.get_word())
-
+        
 if __name__ == '__main__':
     word = Words()
     print(word.get_word())
-
-# get_word_from_file()
