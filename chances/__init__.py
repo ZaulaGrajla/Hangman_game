440550c2a73955609512ef2252848592b527e47b
Paulina Grajewska <56506556+ZaulaGrajla@users.noreply.github.com>
2020-01-14 18:29:07 +0100

    Update __init__.py

--- chances/__init__.py
+++ chances/__init__.py
@@ -1,6 +1,3 @@
 from .chances import Chances
 from .chancesError import ChancesError
 
-# poczytaj o funkcjonalnościach __init__
-# fajnie jak ma importy
-# można tu jeszcze wprowadzić if __name__...
