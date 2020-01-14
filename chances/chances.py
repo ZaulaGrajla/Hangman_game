98c4a0f36fa40abcf4bf5a42a3559211aecb394c
Paulina Grajewska <56506556+ZaulaGrajla@users.noreply.github.com>
2020-01-14 18:28:36 +0100

    Update chances.py

--- chances/chances.py
+++ chances/chances.py
@@ -2,7 +2,6 @@ from .chancesError import ChancesError
 
 class Chances:
     def __init__(self, chances=9):
-        # self.chances=9 if chances is None else chances
         self.chances = chances
 
     def get_chances(self):
@@ -12,11 +11,4 @@ class Chances:
         self.chances-=1
 
         if self.chances==0:
-            raise ChancesError('Nie masz już szans noobie')
-
-# Class chances:
-# w init tylko liczba szans
-#  get chances i decrease chances jako funkcje
-#  w decrease podnoszę błąd jak już nie ma szans
-
-# poczytaj o @property
+            raise ChancesError('Your out of chances!')
